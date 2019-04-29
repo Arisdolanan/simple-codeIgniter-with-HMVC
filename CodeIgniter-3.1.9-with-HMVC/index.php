68fc36fb4cede28a1a1a83a6786d80bf7d7e28c9
Aris Kurniawan <arisxf@gmail.com>
2019-04-30 02:43:55 +0700

    Update index.php date timezone

--- CodeIgniter-3.1.9-with-HMVC/index.php
+++ CodeIgniter-3.1.9-with-HMVC/index.php
@@ -63,6 +63,11 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
+
+// Date Timezone Indonesia
+date_default_timezone_set('Asia/Jakarta');
+// Date Timezone Indonesia
+
 switch (ENVIRONMENT)
 {
 	case 'development':
